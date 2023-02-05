3502d301dd0f13adc84c837ff4f1a8199c4ce1d2
SFYLL <santiagoflood@hotmail.fr>
2023-02-05 18:00:47 +0100

    make other thesis private

--- thesis_monitoring/us_inflation_fed_reaction_function.py/us_inflation_reaction_function.py
+++
@@ -1,9 +0,0 @@
-"""Core Service Inflation!
-Basically three buckets:
-- goods and energy
--housing: which first housing prices thrickling down to rent prices which ultimately impacts shelter inflation
-- core services
-
-proxy funds rate from Andrew Foerster which also takes into account forward guidance and balance-sheet policy"""
-
-"""signs of price-wage spiral, inflation expectations"""
