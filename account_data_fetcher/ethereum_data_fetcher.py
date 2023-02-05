81b706b982c5b4d70b87caf24b617bf336180b95
SFYLL <santiagoflood@hotmail.fr>
2023-02-05 17:53:15 +0100

    etherscan fetcher query smartly

--- account_data_fetcher/ethereum_data_fetcher.py
+++ account_data_fetcher/ethereum_data_fetcher.py
@@ -1,9 +1,39 @@
-import os
-import requests
-from typing import Dict, Callable
+import dataclasses
+from datetime import timezone, datetime, timedelta
 import logging
+import os
+from typing import Callable, Dict, List, Optional
 
 from utilities.account_data_fetcher_base import accountFetcherBase
+from utilities.request_handler import requestHandler
+
+
+@dataclasses.dataclass(init=True, eq=True, repr=True)
+class balanceMetaData:
+    timestamp: datetime
+    balance_per_coin: Dict[str, float]
+
+    def is_acceptable_timestamp_detla(self, delta_in_seconds_allowed) -> bool:
+
+            dt = datetime.utcnow()
+
+            delta: timedelta = self.timestamp - dt
+
+            return delta.total_seconds() < delta_in_seconds_allowed
+
+
+@dataclasses.dataclass(init=True, eq=True, repr=True)
+class priceMetaData:
+    timestamp: datetime
+    prices_per_coin: Dict[str, Dict[str, float]]
+
+    def is_acceptable_timestamp_detla(self, delta_in_seconds_allowed) -> bool:
+
+            dt = datetime.utcnow()
+
+            delta: timedelta = self.timestamp - dt
+                
+            return delta.total_seconds() < delta_in_seconds_allowed
 
 
 class ethereumDataFetcher(accountFetcherBase):
@@ -12,108 +42,40 @@ class ethereumDataFetcher(accountFetcherBase):
     __COIN_BY_ADDRESSES = {"USDC":"0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", "USDT": "0xdAC17F958D2ee523a2206206994597C13D831ec7", "AMPL":"0xD46bA6D942050d489DBd938a2C909A5d5039A161", "DAI": "0x6B175474E89094C44Da98b954EedeAC495271d0F", "DYDX": "0x92D6C1e31e14520e676a687F0a93788B716BEff5"}
     __DECIMAL_BY_COIN = {"USDC": 6, "USDT": 6, "AMPL": 9, "DAI": 18, "DYDX": 18}
 
-    def __init__(self, path: str, password: str) -> None:
+    def __init__(self, path: str, password: str, delta_in_seconds_allowed: int = 30) -> None:
         super().__init__(path, password)
         self.logger = logging.getLogger(__name__) 
-
-    def _api_module(self, url: str) -> str:
-        """Creates an API URL.
-        Overview:
-        ----  
-        Convert relative endpoint (e.g., 'quotes') to full API endpoint.
-        Arguments:
-        ----
-        url (str): The URL that needs conversion to a full endpoint URL.
-        Returns:
-        ---
-        (str): A full URL.
-        """
-
-        return '?'.join([self.__URL, url])
-
-    def _handle_requests(self, url: str, method: str, args: dict = None) -> dict:
-        """[summary]
-        Arguments:
-        ----
-        url (str): [description]
-        method (str): [description]
-        args (dict, optional): [description]. Defaults to None.
-        stream (bool, optional): [description]. Defaults to False.
-        payload (dict, optional): [description]. Defaults to None.
-        Raises:
-        ----
-        ValueError: [description]
-        Returns:
-        ----
-        dict: [description]
-        """
-
-        if method == 'get':
-
-            response = requests.get(
-            url=url, params=args, verify=True)
-
-        elif method == 'post':
-
-            response = requests.post(
-                url=url, params=args, verify=True)
-
-        elif method == 'put':
-
-            response = requests.put(
-                url=url, params=args, verify=True)
-
-        elif method == 'delete':
-
-            response = requests.delete(
-                url=url, params=args, verify=True)
-
-        else:
-            raise ValueError(
-                'The type of request you are making is incorrect.')
-
-        # grab the status code
-        status_code = response.status_code
-
-        # grab the response. headers.
-        response_headers = response.headers
-
-        if status_code == 200:
-
-            if response_headers['Content-Type'] in ['application/json', 'charset=utf-8', "application/json; charset=utf-8"]:
-                return response.json()
-            else:
-                raise Exception("unhandled response type")
-
-        else:
-            # Error
-            print('')
-            print('-'*80)
-            print("BAD REQUEST - STATUS CODE: {}".format(status_code))
-            print("RESPONSE URL: {}".format(response.url))
-            print("RESPONSE HEADERS: {}".format(response.headers))
-            print("RESPONSE TEXT: {}".format(response.text))
-            print('-'*80)
-            print('')
+        self.__request_handler: requestHandler = requestHandler()
+        self.price_meta_data: Optional[priceMetaData] = None
+        self.balance_meta_data: Optional[balanceMetaData] = None
+        self.delta_in_seconds_allowed = delta_in_seconds_allowed #strong api limitations, better not to over query
             
-    def get_total_balance_by_coin(self) -> float:
-        
-        eth_balances: Dict[str:int] = self.get_eth_balances()
+    def __get_total_balance_by_coin(self, delta_in_seconds: int = 120) -> float:
+        if self.balance_meta_data:
+            if self.balance_meta_data.is_acceptable_timestamp_detla(delta_in_seconds):
+                return self.balance_meta_data.balance_per_coin
+
+        eth_balances: Dict[str, int] = self.__get_eth_balances()
         
         eth_balance_gwei: float = 0
         
         for eth_balance in eth_balances:
             eth_balance_gwei += int(eth_balance["balance"]) / 10 ** 18
 
-        token_balances: dict[str: float] = self.get_token_balances_by_coin()
+        token_balances: dict[str, float] = self.__get_token_balances_by_coin()
 
         token_balances["ETH"] = eth_balance_gwei
 
+        self.balance_meta_data = balanceMetaData(
+            timestamp=datetime.utcnow(),
+            balance_per_coin=token_balances
+        )
+
         return token_balances
 
-    def get_eth_balances(self) -> dict:
+    def __get_eth_balances(self) -> dict:
 
-        url_endpoint = self._api_module(url='module=account')
+        url_endpoint = self.__request_handler.api_module(url_base=self.__URL, url_extension='module=account')
         
         params: dict = {
             "action":"balancemulti",
@@ -122,7 +84,7 @@ class ethereumDataFetcher(accountFetcherBase):
             "apikey": self.api_meta_data[self.__EXCHANGE].key
         }
 
-        response = self._handle_requests(
+        response = self.__request_handler.handle_requests(
                    url=url_endpoint,
                    method="get",
                    args=params,
@@ -130,11 +92,10 @@ class ethereumDataFetcher(accountFetcherBase):
 
         return response["result"]
     
-    def get_token_balances_by_coin(self) -> dict:
-        url_endpoint = self._api_module(url='module=account')
+    def __get_token_balances_by_coin(self) -> dict:
+        url_endpoint = self.__request_handler.api_module(url_base=self.__URL, url_extension='module=account')
 
-        balance_by_coin: Dict[str: float] = {}
-
+        balance_by_coin: Dict[str, float] = {}
         
         params: dict = {
             "action":"tokenbalance",
@@ -148,7 +109,7 @@ class ethereumDataFetcher(accountFetcherBase):
             for my_address in self.api_meta_data[self.__EXCHANGE].other_fields["Addresses"]: #free API issues
                 params["contractaddress"] = address
                 params["address"] = my_address
-                result = self._handle_requests(
+                result = self.__request_handler.handle_requests(
                     url=url_endpoint,
                     method="get",
                     args=params,
@@ -161,20 +122,25 @@ class ethereumDataFetcher(accountFetcherBase):
 
         return balance_by_coin
 
-    def get_netliq(self, get_price_from_bybit: Callable) -> float:
-        balance_by_coin: dict = self.get_total_balance_by_coin()
+    def get_netliq(self, get_price_from_coingecko: Callable) -> float:
+        balance_by_coin: dict = self.__get_total_balance_by_coin()
         self.logger.debug(f"{balance_by_coin=}")
+        
         netliq: float = 0
+        
+        self.get_prices_for_coins(balance_by_coin, get_price_from_coingecko)
+        
         for coin, balance in balance_by_coin.items():
             if "USD" in coin:
                 netliq += balance
             else:
-                price = get_price_from_bybit(symbol=coin.upper()+"USDC")["result"]["price"]
+                price = self.price_meta_data.prices_per_coin[coin]["usd"]
                 netliq += float(balance) * float(price)
+        
         return round(netliq,3)
     
-    def get_position(self, get_price_from_bybit: Callable) -> dict:
-        balance_by_coin = self.get_total_balance_by_coin()
+    def get_position(self, get_price_from_coingecko: Callable) -> dict:
+        balance_by_coin = self.__get_total_balance_by_coin()
 
         data_to_return = {
             "Symbol": [],
@@ -183,6 +149,9 @@ class ethereumDataFetcher(accountFetcherBase):
             "Dollar Quantity": []
         } 
 
+
+        self.get_prices_for_coins(balance_by_coin, get_price_from_coingecko)
+
         for coin, balance in balance_by_coin.items():
             if "USD" in coin:
                 data_to_return["Symbol"].append("USD")
@@ -190,13 +159,30 @@ class ethereumDataFetcher(accountFetcherBase):
                 data_to_return["Quantity"].append(round(balance, 3))
                 data_to_return["Dollar Quantity"].append(round(balance,3 ))
             else:
-                price = get_price_from_bybit(symbol=coin.upper()+"USDC")["result"]["price"]
+                price = self.price_meta_data.prices_per_coin[coin]["usd"]
                 data_to_return["Symbol"].append(coin)
                 data_to_return["Multiplier"].append(1)
                 data_to_return["Quantity"].append(round(balance, 3))
                 data_to_return["Dollar Quantity"].append(round(float(balance) * float(price),3))
         
         return data_to_return
+
+    def get_prices_for_coins(self, balance_by_coin: Dict[str, float], get_prices_from_coingecko: Callable) -> None:
+        if self.price_meta_data:
+            if self.price_meta_data.is_acceptable_timestamp_detla(self.delta_in_seconds_allowed):
+                return self.price_meta_data.prices_per_coin
+        
+        #fetching all but stablecoins usd denominated
+        coins_to_fetch_price_for: List[str] = [coin for coin, _ in balance_by_coin.items() if "USD" not in coin]
+
+        price_per_coin = get_prices_from_coingecko(coins_to_fetch_price_for)
+
+        dt = datetime.utcnow()
+
+        self.price_meta_data = priceMetaData(
+            timestamp=dt,
+            prices_per_coin=price_per_coin
+        )
 
 
 if __name__ == '__main__':
