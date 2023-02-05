073af817270a0ddee8abffdc98d4bd3bca13fcec
SFYLL <santiagoflood@hotmail.fr>
2023-02-05 17:53:32 +0100

    ib data fetcher query smartly

--- account_data_fetcher/ib_data_fetcher_flex_queries.py
+++ account_data_fetcher/ib_data_fetcher_flex_queries.py
@@ -1,5 +1,6 @@
 from datetime import timezone, datetime, timedelta
 import logging
+import os
 from time import sleep
 from typing import Optional
 
@@ -30,22 +31,22 @@ class ibDataFetcher(accountFetcherBase):
         }
 
     def update_balance_and_get_ib_datetime(self) -> datetime:
-        if not self.is_acceptable_timestamp_detla(self.balance_object):
+        if not self.is_acceptable_timestamp_detla(self.balance_object, "BALANCE"):
             self.get_balance_object()
         return self.balance_object.whenGenerated
 
     def update_positions_and_get_ib_datetime(self) -> datetime:
-        if not self.is_acceptable_timestamp_detla(self.positions_object):
+        if not self.is_acceptable_timestamp_detla(self.positions_object, "POSITIONS"):
             self.get_positions_object()
         return self.positions_object.whenGenerated
 
     def get_netliq(self) -> float:
-        if not self.is_acceptable_timestamp_detla(self.balance_object):
+        if not self.is_acceptable_timestamp_detla(self.balance_object, "BALANCE"):
             self.get_balance_object()
         return round(float(self.balance_object.ChangeInNAV.endingValue),3)
 
     def get_positions(self) -> dict:
-        if not self.is_acceptable_timestamp_detla(self.positions_object):
+        if not self.is_acceptable_timestamp_detla(self.positions_object, "POSITIONS"):
             self.get_positions_object()
         #denominate in usd so multiply by FXRateToBase, get columns: Symbol, Multiplier, Quantity, MarkPrice, CostBasisPrice, FifoPnlUnrealized
         stmt: OpenPosition = self.positions_object.OpenPositions
@@ -76,22 +77,18 @@ class ibDataFetcher(accountFetcherBase):
         self.positions_object = data.FlexStatements[0]
         self.logger.debug(self.positions_object)
 
-    def is_acceptable_timestamp_detla(self, object_to_check: Optional[FlexStatement]) -> bool:
+    def is_acceptable_timestamp_detla(self, object_to_check: Optional[FlexStatement], query_type: str = "BALANCE") -> bool:
         if not object_to_check:
             return False
 
-        dt = datetime.now(timezone.utc)
+        dt = datetime.utcnow()
 
-        utc_time = dt.replace(tzinfo=timezone.utc)
-
-        adjusted_dt = utc_time + timedelta(hours=self.__HOURS_DIFFERENCE_FROM_UTC)
-
-        if object_to_check == "BALANCE":
-            delta: timedelta = object_to_check.whenGenerated - adjusted_dt
-            return delta.total_seconds > 120
-        if object_to_check == "POSITIONS":
-            delta: timedelta = object_to_check.whenGenerated - adjusted_dt
-            return delta.total_seconds > 120
+        if query_type == "BALANCE":
+            delta: timedelta = object_to_check.whenGenerated - dt + timedelta(hours=self.__HOURS_DIFFERENCE_FROM_UTC)
+            return delta.total_seconds() < 120
+        if query_type == "POSITIONS":
+            delta: timedelta = object_to_check.whenGenerated - dt + timedelta(hours=self.__HOURS_DIFFERENCE_FROM_UTC)
+            return delta.total_seconds() < 120
     
     def get_and_parse_data(self, token, query_id):
         data = self.get_data(token, query_id)
@@ -120,17 +117,15 @@ class ibDataFetcher(accountFetcherBase):
         return cleansed_data
 
 if __name__ == '__main__':
-    numeric_level = getattr(logging, "INFO", None)
-    if not isinstance(numeric_level, int):
-        raise ValueError('Invalid log level: %s' % "INFO")
-    logging.basicConfig(level=numeric_level, format='%(levelname)s:%(asctime)s:%(message)s')
+    from getpass import getpass
+    log_filename= os.path.expanduser("~") + "/log/test.py"
+    numeric_level = getattr(logging, "DEBUG", None)
+    logging.basicConfig(level=numeric_level, format='%(levelname)s:%(asctime)s:%(message)s', filename=log_filename) 
     logger: logging.Logger = logging.getLogger()
-    executor = ibDataFetcher(logger)
-    print(executor.get_positions())
-    print(executor.get_positions())
+    pwd = getpass("provide password for pk:")
+    current_path = os.path.realpath(os.path.dirname(__file__))
+    executor = ibDataFetcher(current_path, pwd)
+    balances = executor.get_netliq()
+    balances = executor.get_netliq()
+    print(balances)
 
-    # data = executor.get_and_parse_data()
-    # print(data.FlexStatements)
-    # stmt = data.FlexStatements[0]
-    # print(stmt.__dict__)
-    # Nav = stmt.ChangeInNAV
