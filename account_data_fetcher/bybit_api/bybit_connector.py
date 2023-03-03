db4ddb32499b2fd60a92319f6bfb87343a4b9d48
SFYLL <santiagoflood@hotmail.fr>
2023-03-03 10:54:15 +0100

    update connector

--- account_data_fetcher/bybit_api/bybit_connector.py
+++ account_data_fetcher/bybit_api/bybit_connector.py
@@ -148,7 +148,7 @@ class bybitApiConnector:
                     status_code=400,
                     time=dt.utcnow().strftime("%H:%M:%S")
                 )
-            print(f"{path=}, {method=} {req_params=} ")
+
             try:
                 raw_response: Response = self.__request_handler.handle_requests(
                     url=path,
