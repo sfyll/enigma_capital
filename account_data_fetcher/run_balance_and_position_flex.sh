a6a69401af8149da94cca622343266055c2f4744
SFYLL <santiagoflood@hotmail.fr>
2023-03-19 17:53:45 +0100

    add rsk run script

--- account_data_fetcher/run_balance_and_position_flex.sh
+++ account_data_fetcher/run_balance_and_position_flex.sh
@@ -13,4 +13,4 @@ cd $ENIGMA
 
 source env/bin/activate
 
-python3 -m account_data_fetcher.runner --log-file ~/log/balance_and_position_fetcher.log --seconds 1800 --request-type FLEX -v -q --manual-balance true --exchange Binance BYBIT DYDX Ethereum IB TradeStation Kraken
+python3 -m account_data_fetcher.runner --log-file ~/log/balance_and_position_fetcher.log --seconds 1800 --request-type FLEX -v -q --manual-balance true --exchange Binance BYBIT DYDX Ethereum IB TradeStation Kraken Rootstock
