c98a8a1c0659653b77fa0dd489e04c2314c892f7
SFYLL <santiagoflood@hotmail.fr>
2023-08-24 16:16:04 +0200

    exchange factory

--- account_data_fetcher/exchanges/exchange_factory.py
+++ account_data_fetcher/exchanges/exchange_factory.py
@@ -9,21 +9,25 @@ class ExchangeFactory:
     __STANDARDIZED_CLASS_NAME = "DataFetcher" 
     
     @classmethod
-    def get_exchange_class(cls, exchange_name, *args, **kwargs) -> ExchangeBase:
+    def get_exchange_class(cls, prefix: str, exchange_name: str, *args, **kwargs) -> ExchangeBase:
         
         if "ib" in exchange_name.lower():
             if exchange_name.endswith("async"):
-                module_name = f"{exchange_name.lower()}.{cls.__STANDARDIZED_FILE_NAME} + _async"
+                module_name = f"{prefix}.{exchange_name.lower()}.{cls.__STANDARDIZED_FILE_NAME} + _async"
             if exchange_name.endswith("flex_queries"):
-                module_name = f"{exchange_name.lower()}.{cls.__STANDARDIZED_FILE_NAME} + _flex_queries"
+                module_name = f"{prefix}.{exchange_name.lower()}.{cls.__STANDARDIZED_FILE_NAME} + _flex_queries"
         else:
-            module_name = f"{exchange_name.lower()}.{cls.__STANDARDIZED_FILE_NAME}"
+            module_name = f"{prefix}.{exchange_name.lower()}.{cls.__STANDARDIZED_FILE_NAME}"
         
         try:
+            print(f"{cls.__STANDARDIZED_FILE_NAME=}")
+            print(f"{module_name=}")
             exchange_module = import_module(module_name)
+            print(f"{exchange_module=}")
             exchange_instance = getattr(exchange_module, cls.__STANDARDIZED_CLASS_NAME)
-        except (ImportError, AttributeError):
-            raise ValueError(f"Unknown or incorrect exchange: {exchange_name}")
+            print(f"{exchange_instance=}")
+        except (ImportError, AttributeError, ModuleNotFoundError) as e:
+            raise Exception(f"{e=}")
 
 
         signature = inspect.signature(exchange_instance.__init__)
