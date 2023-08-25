594c91eae1c49858657f723b4a04c8211d426a3a
SFYLL <santiagoflood@hotmail.fr>
2023-08-25 16:21:08 +0200

    fetch frequency

--- account_data_fetcher/exchanges/exchange_base.py
+++ account_data_fetcher/exchanges/exchange_base.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import json
 import logging
+import time
 from typing import Optional
 
 from setproctitle import setproctitle
@@ -8,34 +9,34 @@ import zmq
 
 class ExchangeBase(ABC):
     __PROCESS_PREFIX = "fetch_"
-    def __init__(self, port_number: int, exchange: str) -> None:
+    def __init__(self, port_number: int, exchange: str, fetch_frequency: int = 60*60) -> None:
         setproctitle(self.__PROCESS_PREFIX + exchange.lower())
         self.port_number = port_number
+        self.fetch_frequency = fetch_frequency
         self.logger = logging.getLogger(__name__)
         self.process_request()
     
     @abstractmethod
-    def fetch_balance(self, accountType: Optional[str] = None):
+    def fetch_balance(self, accountType: Optional[str] = None) -> float:
         pass
 
     @abstractmethod
-    def fetch_positions(self, accountType: Optional[str] = None):
+    def fetch_positions(self, accountType: Optional[str] = None) -> dict:
         pass
 
     def process_request(self):
         context = zmq.Context()
-        socket = context.socket(zmq.REP)
+        socket = context.socket(zmq.PUB)
         socket.bind(f"tcp://*:{self.port_number}")
 
         while True:
-            request_type = socket.recv_string()
-            response_data: str
+            # Fetch balance and positions
+            balance_data = self.fetch_balance()
+            positions_data = self.fetch_positions()
 
-            if request_type == 'balance':
-                response_data = str(self.fetch_balance())
-            elif request_type == 'positions':
-                response_data = json.dumps(self.fetch_positions())
-            else:
-                response_data = f"Unknown request type: {request_type}"
+            # Publish data
+            socket.send_string('balance', str(balance_data))
+            socket.send_string('positions', json.dumps(positions_data))
 
-            socket.send_string(response_data)
+            # Sleep or wait for a signal to fetch the next data
+            time.sleep(self.fetch_frequency) # 1 hours
