904470eb1230effea1b55f43214734c59fd5a168
SFYLL <santiagoflood@hotmail.fr>
2023-08-26 12:37:12 +0200

    exch base config

--- account_data_fetcher/exchanges/exchange_base.py
+++ account_data_fetcher/exchanges/exchange_base.py
@@ -7,14 +7,20 @@ from typing import Optional
 from setproctitle import setproctitle
 import zmq
 
+from infrastructure.log_handler import fetch_logging_config
+
 class ExchangeBase(ABC):
     __PROCESS_PREFIX = "fetch_"
     def __init__(self, port_number: int, exchange: str, fetch_frequency: int = 60*60) -> None:
         setproctitle(self.__PROCESS_PREFIX + exchange.lower())
+        self.exchange: str = exchange
         self.port_number = port_number
         self.fetch_frequency = fetch_frequency
-        self.logger = logging.getLogger(__name__)
-        self.process_request()
+        self.logger = self.init_logging()
+
+    def init_logging(self):
+        fetch_logging_config('/account_data_fetcher/config/logging_config.ini')
+        return logging.getLogger(__name__)
     
     @abstractmethod
     def fetch_balance(self, accountType: Optional[str] = None) -> float:
@@ -34,9 +40,14 @@ class ExchangeBase(ABC):
             balance_data = self.fetch_balance()
             positions_data = self.fetch_positions()
 
-            # Publish data
-            socket.send_string('balance', str(balance_data))
-            socket.send_string('positions', json.dumps(positions_data))
+            msg: dict = {
+                "balance": balance_data,
+                "positions": positions_data 
+            }
+
+            self.logger.debug(f"Sending {self.exchange}: {msg=}")
+
+            socket.send_string(json.dumps(msg))
 
             # Sleep or wait for a signal to fetch the next data
             time.sleep(self.fetch_frequency) # 1 hours
