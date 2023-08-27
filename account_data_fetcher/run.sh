42fffa3afcecae3f3dd694d88a57daa930cb92c7
SFYLL <santiagoflood@hotmail.fr>
2023-08-27 12:44:56 +0200

    runner

--- account_data_fetcher/run.sh
+++ account_data_fetcher/run.sh
@@ -13,4 +13,4 @@ cd $ENIGMA
 
 source env/bin/activate
 
-python3 -m account_data_fetcher.launcher.runner --log-file ~/log/runner.log --seconds 0 -vvv -q --exchanges BYBIT DYDX Ethereum IB_flex Kraken Rsk --writers csv #TradeStation
+python3 -m account_data_fetcher.launcher.runner --log-file ~/log/runner.log --seconds 86400 -vvv -q --exchanges BYBIT DYDX TradeStation Ethereum IB_flex Kraken Rsk --writers csv gsheet
