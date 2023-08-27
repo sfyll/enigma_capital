4028dd952f739ff3f8bcfcd5fc5c3bab9b879ab6
SFYLL <santiagoflood@hotmail.fr>
2023-08-27 12:41:49 +0200

    runner

--- account_data_fetcher/launcher/runner.py
+++ account_data_fetcher/launcher/runner.py
@@ -4,16 +4,18 @@ from getpass import getpass
 import json
 from logging import Logger
 from subprocess import Popen
-from typing import List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple
+
+from account_data_fetcher.data_aggregator.data_aggregator import DataAggregatorConfig, Subscription
 
 from infrastructure.runner_base import RunnerBase
 from infrastructure.api_secret_getter import ApiSecretGetter
 
 
 class Runner(RunnerBase):
-        def __init__(self, logger: Optional[Logger] = None) -> None:
+        def __init__(self, logger: Optional[Logger] = None):
             super().__init__(logger)
-            self.port_per_process, self.secrets_per_process, exchanges = self.outer_set_input_builder(pwd, exchanges)
+            self.port_per_process, self.secrets_per_process = self.outer_set_input_builder(pwd)
 
         @staticmethod
         def get_lower_case_list_elements(list_elements: List[str]) -> List[str]:
@@ -30,7 +32,7 @@ class Runner(RunnerBase):
                 cache_state = True, 
                 refresh_enabled = True"""
         def outer_set_input_builder(self, pwd: str) -> Tuple[dict, dict]:
-            port_per_process = self.__read_json()
+            port_per_process = self.get_port_number_pairings()
             
             secrets_per_process = self.get_secrets(pwd)
 
@@ -38,81 +40,76 @@ class Runner(RunnerBase):
                     {k.lower(): v for k, v in secrets_per_process.items()},
                     )
 
-        def get_secrets(self, pwd: str, path) -> dict:
+        def get_secrets(self, pwd: str) -> dict:
             path = self.base_path + "/account_data_fetcher/secrets/"
             
             secrets: dict = ApiSecretGetter.get_api_meta_data(path, pwd)
 
+            secrets["gsheet"] = ApiSecretGetter.get_gsheet_meta_data(path, pwd)
+
             return secrets
             
-        def launch_exchange_processes(self, pwd: str, exchanges: List[str]) -> None:
-            exchanges = self.get_lower_case_list_elements(process)
+        def launch_processes(self, pwd: str, process_type: str, process_names: List[str], factory_prefix: str) -> None:
+            process_names = self.get_lower_case_list_elements(process_names)
             
-            for exchange_name in exchanges:
+            for process_name in process_names:
                 #two IB implementations!
-                exchange_key = "ib" if "ib" in exchange_name.lower() else exchange_name
-                
+                secret_keys = "ib" if "ib" in process_name.lower() else process_name
                 try:
-                    secrets_json = json.dumps(asdict(self.secrets_per_process[exchange_key]))
+                    secrets_json = json.dumps(asdict(self.secrets_per_process[secret_keys]))
                 except KeyError as e:
-                    if str(e) == "rsk":
-                        # Populate secrets_json with dummy variables if the KeyError is for 'rsk'
+                    if str(e) in ["'csv'", "'rsk'"]:
                         secrets_json = json.dumps({"key": "dummy_key", "secret": "dummy_secret", "other_fields": {}})
-                        self.logger.info(f"KeyError for 'rsk' encountered. Using dummy secrets.")
+                        self.logger.info(f"KeyError for {e} encountered. Using dummy secrets.")
                     else:
-                        # Re-raise the exception for any other KeyError
                         raise
-                
+
                 command = [
                     "python3", "-m",
-                    "account_data_fetcher.exchanges.exchange_factory",
-                    "--exchange_name", exchange_name,
+                    f"account_data_fetcher.{factory_prefix}.process_factory",
+                    f"--{process_type}_name", process_name,
                     "--kwargs", json.dumps({
-                        "port_number": self.port_per_process[exchange_name],
+                        "port_number": self.port_per_process[process_name],
                         "secrets": secrets_json,
-                        "password": pwd
+                        "password": pwd,
+                        "data_aggregator_port_number": self.port_per_process["dataaggregator"]
                     })
                 ]
 
                 process = Popen(command)
+                self.logger.info(f"Started process for {process_name} with PID {process.pid}")
 
-                self.logger.info(f"Started process for {exchange_name} with PID {process.pid}")
+            self.logger.info(f"All {process_type} processes started. Exiting Runner Process.")
 
-            self.logger.info("All processes started. Exiting Runner Process.")
+        def launch_data_aggregator(self, process_name: str, time_interval: int, exchanges: List[str], writers: List[str]) -> None:
+            exchanges, writers = self.get_lower_case_list_elements(exchanges), self.get_lower_case_list_elements(writers)
+            exchange_subscriptions: Dict[str, int] = {
+                "ib" if k in ["ib_flex", "ib_async"] else k: Subscription(port_number=self.port_per_process[k])
+                for k in exchanges if k in self.port_per_process
+            }
+            writer_subscriptions: Dict[str, int] = {k: Subscription(port_number=self.port_per_process[k]) for k in writers if k in self.port_per_process}
+            
 
-        def launch_writer_processes(self, pwd: str, writers: List[str]) -> None:  
-            writers = self.get_lower_case_list_elements(process)          
-            for writer_name in writers:
-                #two IB implementations!                
-                try:
-                    secrets_json = json.dumps(asdict(self.secrets_per_process[writer_name]))
-                except KeyError as e:
-                    if str(e) == "csv":
-                        # Populate secrets_json with dummy variables if the KeyError is for 'rsk'
-                        secrets_json = json.dumps({"key": "dummy_key", "secret": "dummy_secret", "other_fields": {}})
-                        self.logger.info(f"KeyError for 'rsk' encountered. Using dummy secrets.")
-                    else:
-                        # Re-raise the exception for any other KeyError
-                        raise
-                
-                command = [
+            data_aggregator_config: DataAggregatorConfig = DataAggregatorConfig(
+                fetcher_routes=exchange_subscriptions,
+                writer_routes=writer_subscriptions,
+                data_aggregator_port_number=self.port_per_process["dataaggregator"],
+                aggregation_interval=time_interval
+            )
+
+            command = [
                     "python3", "-m",
-                    "account_data_fetcher.writers.writer_factory",
-                    "--writer_name", writer_name,
+                    f"account_data_fetcher.{process_name}.data_aggregator",
                     "--kwargs", json.dumps({
-                        "port_number": self.port_per_process["DataAggregator"],
-                        "secrets": secrets_json,
-                        "password": pwd
+                        "data-aggregator-config": json.dumps(asdict(data_aggregator_config)),
                     })
                 ]
 
-                process = Popen(command)
+            process = Popen(command)
 
-                self.logger.info(f"Started process for {writer_name} with PID {process.pid}")
+            self.logger.info(f"Started process for {process_name} with PID {process.pid}")
 
-            self.logger.info("All processes started. Exiting Runner Process.")
-             
-        def __read_json(self, path_extension: str) -> list:
+        def get_port_number_pairings(self) -> list:
             """FORMAT OF meta_data.json:
             {"addresses": [] }"""
 
@@ -123,31 +120,31 @@ class Runner(RunnerBase):
 
 if __name__ == "__main__":
     import argparse
-    from infrastructure.log_handler import args_handler
+    from infrastructure.log_handler import logging_handler
     parser = argparse.ArgumentParser(
     description="Get Data Of Interest",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--seconds', dest="seconds", type=int, nargs='?', default=10)
-    parser.add_argument('--manual-balance', dest="manual_balance", type=bool, nargs='?', default=False,
-                        help="If activated, will read from a file a manual balance to offset netliq, positive for adding and negative to substract values")
     parser.add_argument("-q","--quiet",action="count",default=0,
                     help="Be more quiet.")
     parser.add_argument("-v", "--verbose",action="count",default=0,
                     help="Be more verbose. Both -v and -q may be used multiple times.")
     parser.add_argument('--log-file', dest="log_file", type=str, nargs='?')
-    parser.add_argument('--exchange', dest="exchange_list", type=str, nargs='+', help="List of exchange to fetch for")
-    parser.add_argument('--writer', dest="writer_list", type=str, nargs='+', help="List of writers")
+    parser.add_argument('--exchanges', dest="exchanges", type=str, nargs='+', help="List of exchange to fetch for")
+    parser.add_argument('--writers', dest="writers", type=str, nargs='+', help="List of writers")
     
     args = parser.parse_args()
 
-    args = args_handler(args)
-
+    args = logging_handler(args)
     pwd = getpass("provide password for pk:")
 
     executor = Runner()
     
     logging.info("Launching Process")
 
-    # executor.launch_exchange_processes(pwd, args.exchange_list)
+    executor.launch_data_aggregator("data_aggregator", args.seconds, args.exchanges, args.writers)
 
-    execut
+    executor.launch_processes(pwd, 'writer', args.writers, 'writers')
+
+    executor.launch_processes(pwd, 'exchange', args.exchanges, 'exchanges')
+
