488077341b5e94ed8d9a3c441d8a86250dc07e05
SFYLL <santiagoflood@hotmail.fr>
2023-09-02 11:21:17 +0200

    example encryptor

---
+++ account_data_fetcher/secrets/example_.api_enc.py
@@ -0,0 +1,19 @@
+{'TradeStation': 
+    {'Key': 'value', 'Secret': 'value', 'Other_fields': {'Username': 'value'}}, 
+'FTX': 
+    {'Key': 'value', 'Secret': 'value'}, 
+'Binance': 
+    {'Key': 'value', 'Secret': 'value'}, 
+'DYDX': 
+    {'Secret': 'value', 'Key': 'value', 'Other_fields': {'Passphrase': 'value'}}, 
+'IB': 
+    {'Key': 'value', 'Secret': 'value', 'Other_fields': {'Endpoint': 'FLEX', 'Token': 'value', 'Balance_query_id': 'value', 'Position_query_id': 'value'}}, 
+'BYBIT': 
+    {'Key': 'value', 'Secret': 'value', 'Other_fields': {}}, 
+'Etherscan': 
+    {'Key': 'value', 'Secret': '', 'Other_fields': {'Addresses': ['value', 'value', '...']}}, 
+'Kraken': 
+    {'Key': 'value', 'Secret': 'value', 'Other_fields': {}}, 
+'Ethereum': 
+    {'Key': 'value', 'Secret': '', 'Other_fields': {}}
+}
