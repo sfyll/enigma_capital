7f620f6623558fe66f1219ddcaa637e37ea98bd9
SFYLL <santiagoflood@hotmail.fr>
2023-09-02 12:04:18 +0200

    comments

--- infrastructure/api_secret_getter.py
+++ infrastructure/api_secret_getter.py
@@ -8,17 +8,45 @@ import utilities.encryptor as encryptor
 
 @dataclasses.dataclass(init=True, eq=True, repr=True)
 class ApiMetaData:
+    """
+    Dataclass for storing API metadata.
+    
+    Attributes:
+        key (str): API key.
+        secret (str): API secret.
+        other_fields (Optional[dict]): Optional dictionary to store additional fields.
+    """
     key: str
     secret: str
     other_fields: Optional[dict]
 
 
 class ApiSecretGetter:
+    """
+    A class for retrieving encrypted API metadata.
+
+    This class provides methods to decrypt API metadata from
+    given files using a provided password. Supports retrieval
+    of API metadata for generic APIs and Google Sheets.
+    """
     def __init__(self):
         pass    
 
     @staticmethod
     def get_api_meta_data(path: str, pwd: str) -> ApiMetaData:
+        """
+        Retrieves and decrypts API metadata from a file.
+
+        Args:
+            path (str): Path to the encrypted metadata file.
+            pwd (str): Password for decrypting the metadata.
+
+        Returns:
+            ApiMetaData: A dataclass instance containing the decrypted API metadata.
+
+        Raises:
+            FileNotFoundError, NotADirectoryError: Raises if the provided file path is invalid.
+        """
         api_meta_data: dict = {}
         try:
             key = encryptor.get_key_from_current_file(path)
@@ -38,6 +66,22 @@ class ApiSecretGetter:
 
     @staticmethod
     def get_gsheet_meta_data(path: str, pwd: str) -> ApiMetaData:
+        """
+        Retrieves and decrypts Google Sheets API metadata from a file.
+
+        Args:
+            path (str): Path to the encrypted metadata file.
+            pwd (str): Password for decrypting the metadata.
+
+        Returns:
+            ApiMetaData: A dataclass instance containing the decrypted API metadata for Google Sheets.
+
+        Raises:
+            FileNotFoundError, NotADirectoryError: Raises if the provided file path is invalid.
+
+        Notes:
+            - The 'key' and 'secret' fields in ApiMetaData will be empty as they are not applicable for Google Sheets.
+        """
         try:
             key = encryptor.get_key_from_current_file(path)
             encrypted_meta_data = encryptor.get_encrypted_gsheet_meta_data(path)
