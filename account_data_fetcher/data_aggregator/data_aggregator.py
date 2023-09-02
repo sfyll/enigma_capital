39f7858db69d8e229889da2ccd58631902ef61bc
SFYLL <santiagoflood@hotmail.fr>
2023-09-02 11:50:00 +0200

    comments and todos

--- account_data_fetcher/data_aggregator/data_aggregator.py
+++ account_data_fetcher/data_aggregator/data_aggregator.py
@@ -4,7 +4,6 @@ import json
 import logging
 import logging.config
 import os
-import pytz
 import time
 from typing import Dict, List, Optional, Set
 
@@ -15,11 +14,27 @@ from infrastructure.log_handler import fetch_logging_config
 
 @dataclass
 class Subscription:
+    """
+    Class to hold the subscription data for a route.
+
+    Attributes:
+        port_number (str): The port number to use for this subscription.
+        topic (Optional[Set[str]]): Optional topic for data filtering.
+    """
     port_number: str
     topic: Optional[Set[str]] = None
 
 @dataclass
 class DataAggregatorConfig:
+    """
+    Configuration for the Data Aggregator.
+
+    Attributes:
+        fetcher_routes (Dict[str, Subscription]): Mapping of exchange names to subscriptions for fetchers.
+        writer_routes (Dict[str, Subscription]): Mapping of writer types to subscriptions for writers.
+        data_aggregator_port_number (int): Port number for the Data Aggregator.
+        aggregation_interval (int): Time interval for data aggregation, in seconds.
+    """
     fetcher_routes: Dict[str, Subscription]  # Mapping of exchange name to port_numbers for subscribing to fetchers
     writer_routes: Dict[str, Subscription]   # Mapping of writer type to port_numbers for publishing to writers
     data_aggregator_port_number: int
@@ -27,38 +42,98 @@ class DataAggregatorConfig:
 
 @dataclass
 class BalanceData:
+    """
+    Class to represent balance data.
+
+    Attributes:
+        value (float): The value of the balance.
+    """
     value: float
 
     def update(self, value: float):
+        """
+        Update balance value.
+
+        Parameters:
+            value (float): New balance value.
+        """
         self.value = value
 
 @dataclass
 class PositionData:
+    """
+    Class to represent position data.
+
+    Attributes:
+        data (Dict[str, List]): A dictionary containing the position data.
+    """
     data: Dict[str, List]
-    
+
     def update(self, value: Dict[str, List]):
+        """
+        Update position data.
+
+        Parameters:
+            value (Dict[str, List]): New position data.
+        """
         for key, val in value.items():
             self.data[key] = val
 
 @dataclass
 class ExchangeData:
+    """
+    Class to hold data relevant to a specific exchange.
+
+    Attributes:
+        balance (BalanceData): The balance data.
+        position (PositionData): The position data.
+        last_fetch_timestamp (float): The last time the data was fetched, in Unix timestamp.
+    """
     balance: BalanceData = field(default_factory=lambda: BalanceData(0))
     position: PositionData = field(default_factory=PositionData)
     last_fetch_timestamp: float = 0
 
     def update(self, balance_and_positions_dict: dict) -> None:
+        """
+        Update balance and position data.
+
+        Parameters:
+            balance_and_positions_dict (dict): Dictionary containing the new balance and position data.
+        """
         self.update_balance(balance_and_positions_dict["balance"])
         self.update_position(balance_and_positions_dict["positions"])
         self.last_fetch_timestamp = time.time()
 
     def update_balance(self, value: float):
+        """
+        Update balance data.
+
+        Parameters:
+            value (float): New balance value.
+        """
         self.balance.update(value)
 
     def update_position(self, value: Dict[str, List]):
+        """
+        Update position data.
+
+        Parameters:
+            value (Dict[str, List]): New position data.
+        """
         self.position.update(value)
 
 @dataclass
 class AggregatedData:
+    """
+    Class to hold aggregated data across exchanges.
+
+    Attributes:
+        aggregation_interval (int): The time interval for aggregation, in seconds.
+        data_routes (DataAggregatorConfig): Configuration for the Data Aggregator.
+        netliq (float): The net liquidation value.
+        date (str): The last date data was sent.
+        exchanges (Dict[str, ExchangeData]): Data for individual exchanges.
+    """
     aggregation_interval: int
     data_routes: DataAggregatorConfig
     netliq: float = 0
@@ -66,10 +141,23 @@ class AggregatedData:
     exchanges: Dict[str, ExchangeData] = field(default_factory=dict)
 
     def get_object_if_ready(self) -> Optional[dict]:
+        """
+        Get the aggregated data object if conditions for sending are met.
+
+        Returns:
+            Optional[dict]: The aggregated data object or None.
+        """
         if self.__can_send():
             return self.__get_object_to_send()
 
     def __can_send(self) -> bool:
+        """
+        Check if conditions for sending data are met.
+        Requires all exchanges to have been populated, and current timestamp - last sent timestamp > aggregation_interval.
+
+        Returns:
+            bool: True if data can be sent, False otherwise.
+        """
         if set(self.data_routes.fetcher_routes.keys()) <= set(self.exchanges.keys()) :
             pass
         else:
@@ -83,13 +171,15 @@ class AggregatedData:
                 return True
 
         else:
-            oldest_fetch_timestamp = min(
-                exchange.last_fetch_timestamp for exchange in self.exchanges.values()
-            )
-
-            return time.time() - oldest_fetch_timestamp >= self.aggregation_interval    
+            return time.time() - min(exchange.last_fetch_timestamp for exchange in self.exchanges.values()) >= self.aggregation_interval    
 
     def __is_new_day(self) -> bool:
+        """
+        Check if it is a new day based on UTC time. Twicked for Interactive Brokers for which new days (and FLEX report publishing) is around 5am UTC.
+ 
+        Returns:
+            bool: True if it is a new day, False otherwise.
+        """
         utc_time = datetime.utcnow()
         
         if utc_time.hour < 6:  # Wait until 7 AM UTC
@@ -101,6 +191,12 @@ class AggregatedData:
         return current_date != last_sent_date
         
     def __get_object_to_send(self) -> dict:
+        """
+        Prepare the object to be sent, updating necessary fields.
+
+        Returns:
+            dict: The aggregated data object.
+        """
         self.netliq = sum(exchange.balance.value for exchange in self.exchanges.values())
         self.date = time.strftime('%Y-%m-%d %H:%M:%S')
 
@@ -128,7 +224,17 @@ class AggregatedData:
         return to_send_object
     
 class DataAggregator:
+    """Main class for aggregating data from multiple exchanges and publishing it to the writers.
+
+    Attributes:
+        aggregated_data (AggregatedData): Data to be aggregated and sent.
+    """
     def __init__(self, config: DataAggregatorConfig) -> None:
+        """Initializes DataAggregator class with configurations.
+        
+        Args:
+            config (DataAggregatorConfig): The configuration settings for the data aggregator.
+        """
         setproctitle("data_aggregator")
         self.logger = self.init_logging()
         self.path = os.path.realpath(os.path.dirname(__file__))
@@ -138,10 +244,33 @@ class DataAggregator:
                                                                      data_routes=config)
 
     def init_logging(self):
+        """Initializes logging for the class.
+
+        Returns:
+            logging.Logger: Configured logger.
+        """
         fetch_logging_config('/account_data_fetcher/config/logging_config.ini')
         return logging.getLogger(__name__)
     
     def run(self):
+        """
+        The main method that handles the data aggregation and publication.
+        
+        This method performs the following operations:
+        1. Initializes the ZeroMQ context.
+        2. Creates and binds a publisher socket for sending aggregated data.
+        3. Creates and connects a subscriber socket for receiving data from various exchanges.
+        4. Enters an infinite loop that:
+            a. Receives data from the subscriber socket.
+            b. Updates the internal aggregated data structure.
+            c. Sends the aggregated data through the publisher socket when ready. That is, when all exchanges have been published and aggregation interval has been met.
+        
+        Note: 
+        - The ZMQ PUB-SUB model is being used here. The publisher (PUB) will send data to any
+        connected subscriber (SUB).
+        - This method assumes that each message received from a subscriber contains both 
+        balance and position data, identified by an "exchange" field.
+        """
         context = zmq.Context()
 
         # Create and bind the publisher for the writers
@@ -154,12 +283,13 @@ class DataAggregator:
         for exchange_name, exchange_subscription in self.aggregated_data.data_routes.fetcher_routes.items():
             self.logger.debug(f"Subscribing data_aggregator to {exchange_name} at tcp://localhost:{exchange_subscription['port_number']}")
             sub_socket.connect(f"tcp://localhost:{exchange_subscription['port_number']}")
-            """TODO: Be able to subscribe to different topics, so that the data_aggregator
-                     can scale easily to more usecases"""
+            #TODO: Be able to subscribe to different topics, so that the data_aggregator can scale easily to more usecases
             sub_socket.setsockopt_string(zmq.SUBSCRIBE, "")
             # for topic in exchange_subscription["topic"]:
             #     sub_socket.subscribe(topic)
 
+        #TODO: Handle failure. What if an exchange stop publishing and we just loop forever?
+        #TODO: Make the function more modular, the data_aggregator should be agnostic to what data is being aggregated as that should be abstracted away.
         while True:
             _, data = sub_socket.recv_multipart()
             balance_and_positions_dict = json.loads(data.decode())
@@ -185,9 +315,7 @@ if __name__ == '__main__':
     import argparse
     import logging
 
-    """TODO:
-        Properly parse the object below so that the inner dataclass
-        can be read as a dataclass, and not accessed as a dict"""
+    #TODO: Properly parse the object below so that the inner dataclass can be read as a dataclass, and not accessed as a dict
     parser = argparse.ArgumentParser()
     parser.add_argument("--kwargs")
     args = parser.parse_args()
--- account_data_fetcher/exchanges/exchange_base.py
+++ account_data_fetcher/exchanges/exchange_base.py
@@ -9,9 +9,28 @@ import zmq
 
 from infrastructure.log_handler import fetch_logging_config
 
+#TODO: For now, we only enforce two methods implementation, namely fetch_balance and fetch_positions. As such, process_request is quite statically defined as well. How could we untangle both so that we can define more abstract methods and have the process_request understands what to fetch dynamically.
 class ExchangeBase(ABC):
+    """
+    Base class for exchange-related data fetching and processing. 
+    It defines which methods have to be implemented by each exchanges.
+    It also handles utilities such as logging as well as other functions common to all child classes.
+
+    Attributes:
+        exchange (str): The name of the exchange, converted to lowercase.
+        port_number (int): The port number for the ZMQ PUB socket.
+        fetch_frequency (int): Time interval for data fetching, in seconds.
+    """
     __PROCESS_PREFIX = "fetch_"
     def __init__(self, port_number: int, exchange: str, fetch_frequency: int = 60*60) -> None:
+        """    
+        Initialize the ExchangeBase object.
+
+        Args:
+            port_number (int): The port number for the ZMQ PUB socket.
+            exchange (str): The name of the exchange.
+            fetch_frequency (int, optional): Time interval for data fetching, in seconds. Defaults to 60*60.
+        """
         setproctitle(self.__PROCESS_PREFIX + exchange.lower())
         self.exchange: str = exchange.lower()
         self.port_number = port_number
@@ -19,18 +38,61 @@ class ExchangeBase(ABC):
         self.logger = self.init_logging()
 
     def init_logging(self):
+        """Initializes logging for the class.
+
+        Returns:
+            logging.Logger: Configured logger.
+        """
         fetch_logging_config('/account_data_fetcher/config/logging_config.ini')
         return logging.getLogger(__name__)
     
     @abstractmethod
     def fetch_balance(self, accountType: Optional[str] = None) -> float:
+        """
+        Fetch the account balance from the exchange.
+        Implemented by the child class.
+
+        Args:
+            accountType (Optional[str], optional): The type of account to fetch. Defaults to None.
+
+        Returns:
+            float: The account balance.
+        """
+
         pass
 
     @abstractmethod
     def fetch_positions(self, accountType: Optional[str] = None) -> dict:
+        """
+        Fetch the account position dictionry from the exchange.
+        Implemented by the child class.
+
+        Args:
+            accountType (Optional[str], optional): The type of account to fetch. Defaults to None.
+
+        Returns:
+            Dict: The account position dictionary.
+        """
         pass
 
     def process_request(self):
+        """
+        Continuously fetches balance and position data from an exchange and publishes it using zmq.
+
+        This function sets up a zmq.PUB socket and then enters an infinite loop. 
+        In each iteration, it fetches the balance and positions from the exchange, 
+        constructs a message, and publishes it to the specified port.
+        Iteration gaps are defined by self.fetch_frequency.
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            Exception: Logs the exception and its details if any part of the process fails.
+        """
         try:
             context = zmq.Context()
             socket = context.socket(zmq.PUB)
