b88a7632f8e8cd4a30a9bb9919856bfd6d3767ce
SFYLL <santiagoflood@hotmail.fr>
2023-09-05 10:39:55 +0200

    todo

--- monitor/portfolio_monitor/netliq_to_telegram.py
+++ monitor/portfolio_monitor/netliq_to_telegram.py
@@ -8,7 +8,7 @@ from tabulate import tabulate
 
 from utilities.telegram_handler import telegramHandler
 
-
+#TODO: Format for phone recipient !
 class netliqToTelgram(telegramHandler):
     pd.set_option('display.max_columns', None)
     pd.set_option('display.max_rows', None)
